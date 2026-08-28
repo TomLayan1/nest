fc8a16e7c3831353ffab3c952c4374503c020865
TomLayan1 <afolayantomisin1919@gmail.com>
2026-08-28 14:49:24 +0100

    Added controllers to the user controller file

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // GET /users
+  @Get()
+  findAll() {
+    return []
+  }
+  // GET /users/:id
+  @Get(':id')
+  findOne(@Param(':id') id: string) {
+    return { id }
+  }
+  // @Param('intern') intern: string
+  // POST /users
+  @Post()
+  createUser(@Body() user: {}) {
+    return user
+  }
+  // PATCH /users/:id
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate }
+  }
+  // DELETE /users/:id
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id }
+  }
+}  
