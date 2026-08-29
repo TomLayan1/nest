1028147678bca4498e447434e8756ff6650a76c1
TomLayan1 <afolayantomisin1919@gmail.com>
2026-08-29 13:22:38 +0100

    Updated users services in users controller file

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,32 +3,33 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  // Create an instance of usersService
   constructor(private readonly usersService: UsersService) {}
 
   // GET /users
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return  
+    return this.usersService.findAll(role);
   }
   // GET /users/:id
   @Get(':id')
-  findOne(@Param(':id') id: string) {
-    return { id }
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id);
   }
   // @Param('intern') intern: string
   // POST /users
   @Post()
-  createUser(@Body() user: {}) {
-    return user
+  createUser(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+    return this.usersService.create(user);
   }
   // PATCH /users/:id
   @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate }
+  update(@Param('id') id: string, @Body() updatedUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+    return this.usersService.update(+id, updatedUser);
   }
   // DELETE /users/:id
   @Delete(':id')
   delete(@Param('id') id: string) {
-    return { id }
+    return this.usersService.delete(+id);
   }
 }  
